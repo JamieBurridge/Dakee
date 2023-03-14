7e28a4cd5fce59638f337602c2f7306ab5cf8c65
JamieBurridge <xross_dev@proton.me>
2023-03-14 20:51:44 +0000

    page: add auth page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-  const hasVisited = false;
+  const hasVisited: boolean = false;
 
   const router = useRouter();
 
--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import styles from "./index.module.scss";
 
 export default function welcome() {
+  const [hasTapped, setHasTapped] = useState<boolean>(false);
+
   return (
-    <main className={styles.splash_screen}>
-      <div className={styles.logo_container}>
-        <img src="/assets/logo.svg" alt="Dakee logo" />
-        <p>Live it like the Locals</p>
-      </div>
-      <div className={styles.continue_container}>
-        <p>
-          Tap anywhere to <br /> continue
-        </p>
-      </div>
+    <main
+      className={
+        hasTapped ? styles.splash_screen_clicked : styles.splash_screen
+      }
+      onClick={() => setHasTapped(true)}
+    >
+      {hasTapped ? (
+        <>
+          <div className={styles.logo_container}>
+            <img src="/assets/logo.svg" alt="Dakee logo" />
+          </div>
+          <div className={styles.auth_container}>
+            <h1>
+              Hey, <br /> have we met?
+            </h1>
+            <p>Sign up to start or log in to your account.</p>
+            <button className={styles.button_signup}>Sign up</button>
+            <button className={styles.button_login}>Log in</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.logo_container}>
+            <img src="/assets/logo.svg" alt="Dakee logo" />
+            <p>Live it like the Locals</p>
+          </div>
+          <div className={styles.continue_container}>
+            <p>
+              Tap anywhere to <br /> continue
+            </p>
+          </div>
+        </>
+      )}
     </main>
   );
 }
