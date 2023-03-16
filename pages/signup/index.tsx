34af615fc15725511af1d33eddf6e17b39421ea0
JamieBurridge <xross_dev@proton.me>
2023-03-16 19:15:42 +0000

    functionality: add toggle password visibility

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./index.module.scss";
 import Button from "../../components/Button/Button";
+import { useState } from "react";
 
 export default function Signup() {
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
   return (
     <main className={styles.signup_container}>
       <div>
@@ -21,8 +24,19 @@ export default function Signup() {
         <form>
           <input type="text" name="name" placeholder="Name" />
           <input type="email" name="email" placeholder="E-mail" />
-          <div>
-            <input type="password" name="password" placeholder="Password" />
+          <div className={styles.password_input_container}>
+            <img
+              src={`/assets/${
+                passwordHidden ? "eye_closed.svg" : "eye_open.svg"
+              }`}
+              alt={`Eye ${passwordHidden ? "closed" : "open"}`}
+              onClick={() => setPasswordHidden(!passwordHidden)}
+            />
+            <input
+              type={passwordHidden ? "password" : "text"}
+              name="password"
+              placeholder="Password"
+            />
           </div>
           <Button text="Sign up" link="/tutorial" type="button" />
         </form>
