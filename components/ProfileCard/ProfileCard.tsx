f600ccc998c0fe9e4b26823566d27c77c9af9159
JamieBurridge <xross_dev@proton.me>
2023-04-13 20:31:55 +0100

    component: add profile card component

---
+++ components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./ProfileCard.module.scss";
+
+interface Topics {
+  name: string;
+  icon?: string;
+}
+
+interface ProfileCardInterface {
+  name: string;
+  rating: string;
+  picture: string;
+  topics: Topics[];
+}
+
+export default function ProfileCard({
+  name,
+  rating,
+  picture,
+  topics,
+}: ProfileCardInterface) {
+  return (
+    <div className={styles.profile_card}>
+      <div className={styles.profile_details}>
+        <p className={styles.profile_name}>{name}</p>
+        <p className={styles.profile_rating}>
+          <img src="/assets/star.svg" alt="Star icon" /> {rating}
+        </p>
+      </div>
+      <div className={styles.profile_picture_container}>
+        <img src={picture} alt={`${name}'s profile picture`} />
+      </div>
+      <div className={styles.profile_topics}>
+        <ul>
+          {topics?.map((topic, index) => (
+            <li key={index}>{topic.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.profile_ask_for_advice}>
+        <p>Ask for advice &gt;</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import FiltersOverlay from "../components/FiltersOverlay/FiltersOverlay";
+import ProfileCard from "../components/ProfileCard/ProfileCard";
 import Topbar from "../components/Topbar/Topbar";
 import styles from "./index.module.scss";
-import FiltersOverlay from "../components/FiltersOverlay/FiltersOverlay";
+
+const dummyProfiles = [
+  {
+    name: "Iva Hardy",
+    rating: "4.6",
+    picture: "https://picsum.photos/id/1001/200/180",
+    topics: [{ name: "art" }, { name: "food" }, { name: "music" }],
+  },
+];
 
 export default function Home() {
   // const hasVisited: boolean = false;
@@ -16,6 +25,7 @@ export default function Home() {
   // }, []);
 
   const [showFilterOverlay, setShowFilterOverlay] = useState<boolean>(false);
+  const [searchText, setSearchText] = useState<string>("");
 
   return (
     <main className={styles.main}>
@@ -29,7 +39,11 @@ export default function Home() {
       <div className={styles.content}>
         <h1>Find the Local!</h1>
         <div className={styles.search_input_container}>
-          <input type="text" placeholder="Choose a city..." />
+          <input
+            type="text"
+            placeholder="Choose a city..."
+            onChange={(e) => setSearchText(e.target.value)}
+          />
         </div>
         <div className={styles.search_options}>
           <div onClick={() => setShowFilterOverlay(true)}>
@@ -39,12 +53,27 @@ export default function Home() {
           <div>Search &gt;</div>
         </div>
 
-        <div className={styles.search_empty}>
-          <img src="/assets/magnifying_glass.svg" alt="Magnifying glass icon" />
-          <p>
-            Choose a city <br /> to search for the Locals!
-          </p>
-        </div>
+        {/* If no data */}
+        {!dummyProfiles && (
+          <div className={styles.search_empty}>
+            <img
+              src="/assets/magnifying_glass.svg"
+              alt="Magnifying glass icon"
+            />
+            <p>
+              Choose a city <br /> to search for the Locals!
+            </p>
+          </div>
+        )}
+
+        {/* If data */}
+        {dummyProfiles && (
+          <div className={styles.search_results}>
+            {dummyProfiles?.map((profile, index) => (
+              <ProfileCard key={index} {...profile} />
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
